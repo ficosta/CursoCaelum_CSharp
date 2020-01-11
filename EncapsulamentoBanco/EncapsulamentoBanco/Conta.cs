e4e78d62985fdcd36054d5447cd39e8d073a267d
Felipe Iasi <felipeiasi@gmail.com>
2020-01-11 16:55:44 -0300

    Encapsulamento Classe

---
+++ EncapsulamentoBanco/EncapsulamentoBanco/Conta.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulamentoBanco
+{
+    class Conta
+    {
+        public int Numero { get; set; }
+        public double Saldo { get; private set; }
+
+
+        public void Saca(double valor)
+        {
+            if(Saldo >= valor)
+            {
+                Saldo -= valor;
+            }
+        }
+
+       public void Deposito(double valor)
+        {
+            if (valor > 0)
+                Saldo += valor;
+
+        }
+    }
+}
---
+++ EncapsulamentoBanco/EncapsulamentoBanco/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EncapsulamentoBanco
+{
+    public partial class Form1 : Form
+    {
+        Conta conta = new Conta();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void button2_Click(object sender, EventArgs e)
+        {
+            conta.Saca(10);
+            atualizaSaldoTela();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            conta.Saca(50);
+            atualizaSaldoTela();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            conta.Deposito(10);
+            atualizaSaldoTela();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            conta.Deposito(50);
+            atualizaSaldoTela();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"O seu saldo é:{conta.Saldo}");
+        }
+        void atualizaSaldoTela()
+        {
+            label1.Text = $"O seu saldo é:{conta.Saldo}";
+        }
+    }
+}
